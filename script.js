e297d02f18e57399055746c04560b932f5a0eb78
Guido Cardarelli <guidocarda@hotmail.com>
2022-05-02 18:31:10 -0300

    Bug fixes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var swiperPopular = new Swiper(".popular__section", {
-  slidesPerView: 1,
+  slidesPerView: 1.5,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
   breakpoints: {
-    // when window width is >= 320px
-    600: {
+    // when window width is >= 400px
+    400: {
       slidesPerView: 2,
       spaceBetween: 20
     },
-    // when window width is >= 480px
+    // when window width is >= 600px
     600: {
       slidesPerView: 3,
       spaceBetween: 20
